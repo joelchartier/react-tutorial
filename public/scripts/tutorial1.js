0b939c0d46427fb05a6e65d56e696ad9756fbf40
Joel Chartier <joelchartier1@gmail.com>
2015-10-10 11:40:51 -0400

    fetch data from server using ajax (jquery) call. updating data from server using polling.

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,19 +1,39 @@
-var data = [
-  {
-    author: "Pete Hunt",
-    text: "This is a comment"
-  }, {
-    author: "Jordan Walke",
-    text: "This is *another* comment"
-  }
-];
-
 var CommentBox = React.createClass({
+
+  loadCommentsFromServer: function () {
+    // TODO: Should replace this by a REST call
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({
+          data: data
+        });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function () {
+      return {
+        data: []
+      };
+  },
+
+  componentDidMount: function () {
+      this.loadCommentsFromServer();
+      // TODO: Should replace this by a WS, instead of using polling
+      setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
   render: function () {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={ this.props.data}/>
+        <CommentList data={ this.state.data}/>
         <CommentForm />
       </div>
     );
@@ -67,6 +87,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={ data }/>,
+  <CommentBox url="/api/comments" pollInterval={ 2000 }/>,
   document.getElementById('content')
 );
